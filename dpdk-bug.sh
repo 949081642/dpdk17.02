95df37794f2d290cdde7886cb77198b00d70339a
949081642 <949081642@qq.com>
2018-05-15 10:22:43 +0800

    mianmi

--- dpdk-bug.sh
+++
@@ -1,3 +0,0 @@
-sed -i "s/pci_intx_mask_supported(dev)/pci_intx_mask_supported(dev)||true/g" lib/librte_eal/linuxapp/igb_uio/igb_uio.c
-patch -p 1 < 0001-PATCH-kni-use-netlink-event-for-multicast-driver-par.patch
-
